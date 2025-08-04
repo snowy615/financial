f8ffb344f31bdcc7b37ea318a195280062cf96c9
snowy615 <yansnow615@gmail.com>
2025-08-03 20:54:03 -0700

    import libraries but not working

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import pandas as pd
+import numpy as np
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import yfinance as yf
 
+import cufflinks as cf
+cf.set_config_file(offline=True)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import warnings
+warnings.filterwarnings('ignore')
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+pd.__version__, cf.__version__, yf.__version__
